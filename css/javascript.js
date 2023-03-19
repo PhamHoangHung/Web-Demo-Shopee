eb116d63242e38718761d75759625fb6a9fe7d33
PhamHoangHung <96969032+PhamHoangHung@users.noreply.github.com>
2023-03-19 12:24:52 +0700

    fix-2-19/3/2023

--- css/javascript.js
+++ css/javascript.js
@@ -244,7 +244,7 @@
             for(let product of products){
                 out += `
                     <div class="showproduct-center_item">
-                        <a href="./assest/pageDetailsProduct/text-DetailProduct/DetailProducts.html">
+                        <a href="./assest/pageDetailsProduct/text-Commit-DetailsProduct/main/textCmIndexDetails.html">
                             <div class="showproduct-center_item--img">
                                 <img src='${product.image}'>
                             </div>
